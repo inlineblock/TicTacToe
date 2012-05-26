669174dae6eef9d0a23b33650052535212c9135c
Dennis Wilkins <dennis@deliciousmorsel.com>
2012-05-26 12:13:34 -0600

    Initial Commit

---
+++ tictactoe/DMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DMAppDelegate.h
+//  tictactoe
+//
+//  Created by Dennis Wilkins on 5/26/12.
+//  Copyright (c) 2012 Delicious Morsel LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tictactoe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tictactoe
+//
+//  Created by Dennis Wilkins on 5/26/12.
+//  Copyright (c) 2012 Delicious Morsel LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DMAppDelegate class]));
+	}
+}
